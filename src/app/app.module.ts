408b1c0fdba5dc8d7bac9d24bc09fced126b3d73
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-21 09:57:59 -0300

    Criação dos componentes na tela inicial
    Criação da tela inicial e de criação de componentes como:
    - Login
    - Register
    - Balance
    - Data entry
    
    Nesse commit está alguns testes básicos e leves edições para que o Front seja desenvolvido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../content/login/login.component';
+import { RegisterComponent } from '../content/register/register.component';
+import { BalanceComponent } from '../content/balance/balance.component';
+import { DataentryComponent } from '../content/dataentry/dataentry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BalanceComponent,
+    DataentryComponent,
   ],
   imports: [
     BrowserModule,
