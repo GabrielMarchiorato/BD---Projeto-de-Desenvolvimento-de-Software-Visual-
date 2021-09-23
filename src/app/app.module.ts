219d21afffad6705d8a87a28c190fc00ad6907db
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2021-09-23 17:59:47 -0300

    adicionando serviços e trocando as pastas de locais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from '../content/login/login.component';
-import { RegisterComponent } from '../content/register/register.component';
-import { BalanceComponent } from '../content/balance/balance.component';
-import { DataentryComponent } from '../content/dataentry/dataentry.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BalanceComponent } from './pages/balance/balance.component';
+import { DataentryComponent } from './pages/dataentry/dataentry.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
