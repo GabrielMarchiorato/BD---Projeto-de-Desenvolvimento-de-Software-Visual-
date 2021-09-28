38b1eae3c949705ff475fd00297c831d65306154
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-28 09:27:54 -0300

    Criando base do CRUD de usuário

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { User } from
 
-@Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
-})
-export class UserComponent implements OnInit {
+// @Component({
+//   selector: 'app-user',
+//   templateUrl: './user.component.html',
+//   styleUrls: ['./user.component.scss']
+// })
+// export class EditarComponent implements OnInit {
 
-  constructor() { }
+//   user : User;
 
-  ngOnInit(): void {
-  }
+//   constructor(private service : UserComponent,
+//       private router: Router, private route : ActivatedRoute) {
+//     this.user = {} as User;
+//   }
 
-}
+//   ngOnInit(): void {
+//     // const str = this.route.snapshot.paramMap.get('id');
+//     // this.service.buscarPorId(Number(str)).then( user => {
+//     //   this.user = user;
+//     // });
+//   }
+
+//   // atualizar(user : User) {
+//   //   this.service.atualizarUsuario(user).then(() => {
+//   //     console.log('Usuario atualizado');
+//   //     this.router.navigate(['/admin']);
+//   //   });
+//   // }
+// }
+
+
+
+
