d642df3899020e01844ef1bfd96fbbcf34234d3e
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-28 21:21:53 -0300

    Adição de botões na aba de seleção de cartão

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem, PrimeNGConfig } from "primeng/api";
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent{
 
-  constructor() { }
+  cardOptions: any[];
+  value1: string = "off";
+  value2: string = "off";
 
-  ngOnInit(): void {
+  constructor(private primeNGConfig: PrimeNGConfig) {
+
+
+    this.cardOptions = [
+      { name: "Crédito", value: 1 },
+      { name: "Débito", value: 2 }
+    ];
+
+  }
+
+  ngOnInit() {
+    this.primeNGConfig.ripple = true;
   }
 
 }
+
--- src/app/pages/card/card.module.ts
+++ src/app/pages/card/card.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CardRoutingModule } from './card-routing.module';
+import {InputTextModule} from 'primeng/inputtext';
+import { Component } from "@angular/core";
+import { SelectItem, PrimeNGConfig } from "primeng/api";
+import {SelectButtonModule} from 'primeng/selectbutton';
+
 
 import { CardComponent } from './card.component';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    CardRoutingModule
+    CardRoutingModule,
+    InputTextModule,
+    SelectButtonModule
   ],
   declarations: [CardComponent]
 })
 export class CardModule {}
+
+
